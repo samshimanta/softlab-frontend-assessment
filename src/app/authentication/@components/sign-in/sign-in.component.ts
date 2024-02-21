0e3f52f6abc645a4cd8276dcab7e7dac1b5c3318
shimanta <nirobshimanta@gmail.com>
2024-02-22 01:08:18 +0600

    primeng primeflex installation

--- src/app/authentication/@components/sign-in/sign-in.component.ts
+++ src/app/authentication/@components/sign-in/sign-in.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrl: './sign-in.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class SignInComponent {
-
+  hide = true;
 }
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -6,6 +6,12 @@ import { SignInComponent } from './@components/sign-in/sign-in.component';
 import { SignUpComponent } from './@components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './@components/forgot-password/forgot-password.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +21,12 @@ import { ForgotPasswordComponent } from './@components/forgot-password/forgot-pa
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class AuthenticationModule { }
